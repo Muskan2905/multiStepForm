c2e565fdcc625fb2ad3ab7157b32b5b8a80af39b
Muskan2905 <69806508+Muskan2905@users.noreply.github.com>
2023-06-27 09:14:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header'
+import StepByStep from './components/StepByStep';
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <StepByStep />
+    </div>
+  );
+}
+
+export default App;
